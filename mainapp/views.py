c784172489dd5a367347f9d4998455006015d079
ipischurkov <ipischurkov@gmail.com>
2018-01-19 15:20:55 +0300

    Добавлена карточка продукта, прописаны данные в карточку, добавлена динамическая ссылки на товар.

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,11 +4,15 @@ from mainapp.models import Product
 from django.contrib import auth
 from django.http import Http404
 
+
 # Create your views here.
 def main(request):
     product = Product.objects.all()
     product_top = Product.objects.all()[:3]
     product_sale = Product.objects.all()[3:6]
-    
     return render(request, 'mainapp/main.html', {'product': product, 'product_top': product_top, 'product_sale': product_sale})
 
+
+def item(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'mainapp/item.html', {'product': product})
